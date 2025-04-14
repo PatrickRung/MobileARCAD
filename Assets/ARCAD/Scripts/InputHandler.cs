371659085e18371463ed5873b626bfdaa1ea6ed3
PatrickRung <96024158+PatrickRung@users.noreply.github.com>
2025-04-13 23:31:07 -0700

    fixed issue with improper spawning of cube

--- Assets/ARCAD/Scripts/InputHandler.cs
+++ Assets/ARCAD/Scripts/InputHandler.cs
@@ -57,17 +57,20 @@ public class InputHandler : MonoBehaviour
             Debug.Log("creating ray");
             // The direction that the camera is facing (transform.forwards) will always point towards the center
             // of the camera thus making it obselete for our purposes
-            Vector3 cursorWorldPos = playerCam.ScreenToWorldPoint(new Vector3(pointerPosition.ReadValue<Vector2>().x, 
-                    pointerPosition.ReadValue<Vector2>().y, playerCam.nearClipPlane));
-            Debug.DrawRay(cursorWorldPos, Vector3.Cross((playerCam.transform.position - cursorWorldPos), playerCam.transform.up)
-                    , Color.green); 
-            // if (Physics.Raycast(playerCam.ScreenToWorldPoint(pointerPosition.ReadValue<Vector2>()),
-            //         playerCam.transform.forward, out obectHit)) {
-            //     Debug.Log(obectHit.point); 
-            //     GameObject spawnedCube = Instantiate(cubePrefab, objectHolder.transform);
-            //     spawnedCube.transform.position = obectHit.point;
-            //     spawnedCube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-            // }
+            // Vector3 cursorWorldPos = playerCam.ScreenToWorldPoint(new Vector3(pointerPosition.ReadValue<Vector2>().x, 
+            //         pointerPosition.ReadValue<Vector2>().y, playerCam.nearClipPlane));
+
+            Ray ray = playerCam.ScreenPointToRay(pointerPosition.ReadValue<Vector2>());
+            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.green, 2f);
+
+            if (Physics.Raycast(ray.origin,
+                    ray.direction * 100f, out obectHit)) {
+                Debug.Log(obectHit.point); 
+                GameObject spawnedCube = Instantiate(cubePrefab, objectHolder.transform);
+                spawnedCube.transform.position = obectHit.point;
+                spawnedCube.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+            }
+
         }
 
 
