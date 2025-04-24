aab4519820930ce5e009bfeedd63ca6ab994730c
PatrickR <96024158+PatrickRung@users.noreply.github.com>
2025-04-24 16:02:52 -0700

    toggle button for camera
    pressing toggle camera feed turns the camera on and off allowing user to view only the mesh (for debugging)

---
+++ Assets/ARCAD/Scripts/ButtonInputHandler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ButtonInputHandler : MonoBehaviour
+{
+    public ARCameraManager ARCamManager;
+
+    public void toggleCameraFeed() {
+        if(ARCamManager.enabled) {
+            ARCamManager.enabled = false;
+        }
+        else {
+            ARCamManager.enabled = true;
+        }
+    }
+}
